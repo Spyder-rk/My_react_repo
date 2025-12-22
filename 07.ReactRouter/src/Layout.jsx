33aa65f54d0a3b454fa436b6f26d3999ed3a346d
Rajesh Kumar Rout <rajeshkumarrout9977@gmail.com>
2025-12-22 23:56:50 +0530

    React-router config

---
+++ 07.ReactRouter/src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Outlate} from 'react-router'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlate/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
